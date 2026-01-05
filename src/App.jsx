57d24f1b674faf4f843d702e9bd081ceecb6553a
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-01-05 22:13:38 +0530

    Seventh Commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
           </Routes>
         </div>
         <Footer />
